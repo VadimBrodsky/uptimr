1b694041e6c1e7b86ae7b23bd98249f52f398735
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-08-26 22:46:43 -0400

    Add base server

--- js/node/masterclass/uptimr/index.ts
+++ js/node/masterclass/uptimr/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+/*
+ * Primary file for the API
+ *
+ */
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello World\n');
+});
+
+server.listen(3000, () => {
+  console.log('The server is now listening on port 3000');
+})
