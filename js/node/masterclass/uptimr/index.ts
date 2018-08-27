71e59c95310df1ef3072b4931258a6bc326ebc39
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-08-27 19:54:02 -0400

    Parse the request and log

--- js/node/masterclass/uptimr/index.ts
+++ js/node/masterclass/uptimr/index.ts
@@ -3,9 +3,46 @@
  *
  */
 import * as http from 'http';
+import * as sd from 'string_decoder';
+import * as url from 'url';
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
+  // get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // get the path
+  const { pathname } = parsedUrl;
+  const trimmedPath = pathname.replace(/^\/+|\/+$/g, '');
+
+  // get the HTTP method
+  const method = req.method.toLowerCase();
+
+  // get the query string as an object
+  const { query } = parsedUrl;
+
+  // get the headers as an object
+  const { headers } = req;
+
+  // get the payload if any
+  const decoder = new sd.StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    // send the respose
+    res.end('Hello World\n');
+
+    // log the response back
+    console.log(
+      `Request received on path ${trimmedPath} with ${method} method with ${JSON.stringify(
+        query,
+      )} parameters, headers: ${JSON.stringify(headers)}`,
+    );
+  });
 });
 
 server.listen(3000, () => {
