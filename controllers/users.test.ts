cf757f2e00cdacc04b415fa3c30c0377123c9171
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-09-17 22:24:54 -0400

    Finish users.post specs

--- controllers/users.test.ts
+++ controllers/users.test.ts
@@ -1,6 +1,8 @@
 import * as data from '../lib/data';
 import usersController from './users';
 
+jest.mock('../lib/logger');
+
 describe('post', () => {
   it('should create a new user record', async () => {
     const readSpy = jest.spyOn(data, 'read').mockRejectedValue(new Error());
@@ -20,20 +22,47 @@ describe('post', () => {
     expect(res.status).toEqual(200);
   });
 
-  it('should return 400 if missing any of the required fields', async () => {
-    let error;
-    try {
-      await usersController.post({
-        payload: {
-          firstName: 'John',
-          lastName: 'Snow',
-          password: 'knownothing',
-        },
-      });
-    } catch (e) {
-      error = e;
-    }
+  it('should return 400 if missing any of the required fields', () => {
+    const res = usersController.post({
+      payload: {
+        firstName: 'John',
+        lastName: 'Snow',
+        password: 'knownothing',
+      },
+    });
 
-    expect(error.status).toBe(400);
+    expect(res).rejects.toHaveProperty('status', 400);
+  });
+
+  it('should return 400 if the user already exists', () => {
+    const readSpy = jest.spyOn(data, 'read').mockResolvedValueOnce(true);
+
+    const res = usersController.post({
+      payload: {
+        firstName: 'John',
+        lastName: 'Snow',
+        password: 'knownothing',
+        phone: '2261234567',
+        tosAgreement: true,
+      },
+    });
+
+    expect(res).rejects.toHaveProperty('status', 400);
+  });
+
+  it('shuld try return 500 if user creation failed', () => {
+    const readSpy = jest.spyOn(data, 'read').mockRejectedValue(new Error());
+    const createSpy = jest.spyOn(data, 'create').mockRejectedValue(new Error());
+
+    const res = usersController.post({
+      payload: {
+        firstName: 'John',
+        lastName: 'Snow',
+        password: 'knownothing',
+        phone: '2261234567',
+        tosAgreement: true,
+      },
+    });
+    expect(res).rejects.toHaveProperty('status', 500);
   });
 });
