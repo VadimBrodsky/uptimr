6da64c08c679f831af05532cd0f102fb93463c30
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-10-13 23:04:08 -0400

    Move handlers into the router

--- lib/router.ts
+++ lib/router.ts
@@ -1,11 +1,45 @@
-import * as handlers from './handlers';
+import checksController from '../controllers/checks';
+import tokensController from '../controllers/tokens';
+import usersController from '../controllers/users';
+
+const ping = (data) => {
+  return Promise.reject({ code: 200 });
+};
+
+const notFound = (data) => {
+  return Promise.reject({ code: 404 });
+};
+
+const users = (data) => {
+  if (Object.keys(usersController).includes(data.method)) {
+    return usersController[data.method](data);
+  } else {
+    return Promise.reject({ code: 405 });
+  }
+};
+
+const tokens = (data, callback) => {
+  if (Object.keys(tokensController).includes(data.method)) {
+    tokensController[data.method](data, callback);
+  } else {
+    callback(405);
+  }
+};
+
+const checks = (data, callback) => {
+  if (Object.keys(checksController).includes(data.method)) {
+    checksController[data.method](data, callback);
+  } else {
+    callback(405);
+  }
+};
 
 const router = {
-  checks: handlers.checks,
-  ping: handlers.ping,
-  tokens: handlers.tokens,
-  users: handlers.users,
+  checks,
+  ping,
+  tokens,
+  users,
 };
 
 export const matchRoute = (trimmedPath: string) =>
-  Object.keys(router).includes(trimmedPath) ? router[trimmedPath] : handlers.notFound;
+  Object.keys(router).includes(trimmedPath) ? router[trimmedPath] : notFound;
